5407a32e7f3d60aa20942511b47c005673765676
Joaquin Iglesias Coqueti <jiglesiascoqueti@alumnos.exa.unicen.edu.ar>
2022-10-08 11:27:29 -0300

    Cambios en conexión del para free-hostinger

--- app/models/connection_db.php
+++ app/models/connection_db.php
@@ -4,9 +4,9 @@ class ConnectionDB {
     protected $db;
     private const typeDb = 'mysql';
     private const host = 'localhost';
-    private const dbname = 'db_books';
-    private const user = 'root';
-    private const password = '';
+    private const dbname = 'id19679558_db_books';
+    private const user = 'id19679558_root';
+    private const password = '_g)mp9G@/VGhp7mx';
     
     function __construct() {
         $this->db = new PDO(ConnectionDB::typeDb . ":host=" . ConnectionDB::host . ";dbname=" . ConnectionDB::dbname . ";charset=utf8", ConnectionDB::user, ConnectionDB::password);
--- database/database.sql
+++ database/database.sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_books`
+-- Base de datos: `id19679558_db_books`
 --
-CREATE DATABASE IF NOT EXISTS `db_books` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `db_books`;
+CREATE DATABASE IF NOT EXISTS `id19679558_db_books` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `id19679558_db_books`;
 
 -- --------------------------------------------------------
 
